d29917cc8d8c92c607497fb9aa32c10e89242823
valioromanov <valioromanov@gmail.com>
2023-11-04 00:54:04 +0200

    Add presenter tests

--- cmd/calcurator-api/internal/calculator/presenter_test.go
+++ cmd/calcurator-api/internal/calculator/presenter_test.go
@@ -1,9 +1,13 @@
 package calculator_test
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"ft-calculator/cmd/calcurator-api/internal/calculator"
 	"ft-calculator/cmd/calcurator-api/internal/calculator/mocks"
 	"ft-calculator/helper/mockutil"
+	"net/http"
 	"net/http/httptest"
 
 	"github.com/gin-gonic/gin"
@@ -27,4 +31,56 @@ var _ = Describe("Presenter", mockutil.Mockable(func(helper *mockutil.Helper) {
 		presenter = calculator.NewPresenter(controller)
 	})
 
+	Describe("Evaluate", func() {
+		expression := "What is 3 plus 1"
+		When("request is valid", func() {
+			BeforeEach(func() {
+				reqBody := map[string]interface{}{
+					"expression": expression,
+				}
+				mockContext.Request = httptest.NewRequest("POST", "http://abc.com", createBody(reqBody))
+				controller.EXPECT().Evaluate(expression).Return(4, nil)
+			})
+
+			It("returns StatusOK and the result", func() {
+				presenter.Evaluate(mockContext)
+				Expect(mockContext.Writer.Status()).To(Equal(http.StatusOK))
+			})
+		})
+
+		When("request body is empty", func() {
+			BeforeEach(func() {
+				mockContext.Request = httptest.NewRequest("POST", "http://abc.com", nil)
+			})
+
+			It("returns StatusBadRequest", func() {
+				presenter.Evaluate(mockContext)
+				Expect(mockContext.Writer.Status()).To(Equal(http.StatusBadRequest))
+			})
+		})
+
+		When("request body is invalid", func() {
+			BeforeEach(func() {
+				invalidExpression := "What is 3 plus plus 1"
+				reqBody := map[string]interface{}{
+					"expression": invalidExpression,
+				}
+				mockContext.Request = httptest.NewRequest("POST", "http://abc.com", createBody(reqBody))
+				controller.EXPECT().Evaluate(invalidExpression).Return(0, fmt.Errorf("some-error"))
+			})
+
+			It("returns StatusBadRequest", func() {
+				presenter.Evaluate(mockContext)
+				Expect(mockContext.Writer.Status()).To(Equal(http.StatusBadRequest))
+			})
+		})
+	})
+
 }))
+
+func createBody(fields map[string]interface{}) *bytes.Buffer {
+	b, err := json.Marshal(fields)
+	Expect(err).ToNot(HaveOccurred())
+
+	return bytes.NewBufferString(string(b))
+}
